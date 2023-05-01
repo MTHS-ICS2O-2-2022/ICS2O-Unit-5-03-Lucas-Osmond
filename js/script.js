ccef90e340a22d4c4344b65173644cd3fd203194
Lucas-Osmond <124389349+Lucas-Osmond@users.noreply.github.com>
2023-05-01 17:24:23 +0000

    added js code

--- js/script.js
+++ js/script.js
@@ -1,5 +1,22 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2020 Mr Coxall All rights reserved
 //
-// Created by: Mr. Coxall
+// Created by: Mr Coxall
 // Created on: Sep 2020
 // This file contains the JS functions for index.html
+
+"use strict"
+
+/**
+ * This function updates a slider value.
+ */
+function myButtonClicked () {
+  const age = parseInt(document.getElementById("age").value)
+
+  if (age >= 18) {
+    document.getElementById('response').innerHTML = 'You can Watch!'
+  } else if (age >= 13) {
+    document.getElementById('response').innerHTML = 'You can Watch!'
+  } else {
+    document.getElementById('response').innerHTML = 'You cannot Watch!'
+  }
+}
